c746c938cf3aad9900c175e4bd6c05fc9c689c7f
Werner Drasch <werner.drasch@gmail.com>
2018-12-24 10:28:04 +0100

    testing ajax feat

--- mini.js
+++ mini.js
@@ -1,3 +1,11 @@
 simply.title('WD77');
 simply.subtitle('Testing');
-Pebble.showSimpleNotificationOnPebble('WD77','Testing notification from JS...');
+//Pebble.showSimpleNotificationOnPebble('WD77','Testing notification from JS...');
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.name, subtitle: data.main.temp });
+  });
+});
